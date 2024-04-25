304c8b7f5fbc3115bf7d3a06d816be445d5eab12
prodonik <rruzibayevich@gmail.com>
2024-04-25 23:42:04 +0500

    message

--- repo/repository.go
+++ repo/repository.go
@@ -1,16 +1,16 @@
 package repo
 
 import (
-    "os/exec"
-    "strings"
+	"os/exec"
+	"strings"
 )
 
-func GetUserName() (string, error) {
-    cmd := exec.Command("git", "config", "--global", "user.name")
-    output, err := cmd.Output()
-    if err != nil {
-        return "", err
-    }
-    username := strings.TrimSpace(string(output))
-    return username, nil
+func GetUserEmail() (string, error) {
+	cmd := exec.Command("git", "config", "--global", "user.email")
+	output, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	email := strings.TrimSpace(string(output))
+	return email, nil
 }
